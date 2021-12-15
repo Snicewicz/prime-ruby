0b687fb51130da9773485d5713a09624313924a5
Snicewicz <snicewicz@gmail.com>
2021-12-15 09:57:25 -0500

    commit tree 688b6e4ff4c8b5d772ed7853f2ef9525154f04bd

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+def prime?(int)
+    if int <= 1 
+        return false
+    elsif int == 2 
+        return true
+    else 
+        range = (2..(int - 1)).to_a 
+        range.none? { |n| int % n == 0}
+    end
+
+end
